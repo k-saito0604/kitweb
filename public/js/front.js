acab5ad3c928036c9dde76153064bb3c7173ee40
snemesk <sainemesis.ksk@gmail.com>
2020-02-04 17:45:15 +0900

    '200204nulnul'

--- public/js/front.js
+++ public/js/front.js
@@ -78,11 +78,12 @@ function demo () {
 }
 
 $(function(){
-  $('a[href^=#]').click(function(){
-    var speed = 1000;
+  $('a[href^="#"]').click(function(){
+    var adjust = -67;
+    var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
+    var position = target.offset().top + adjust;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
