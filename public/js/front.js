9321b96c01bda5faa58b0d39ab9503265f1dd415
snemesk <sainemesis.ksk@gmail.com>
2020-02-05 14:16:38 +0900

    '200205nuru'

--- public/js/front.js
+++ public/js/front.js
@@ -77,6 +77,19 @@ function demo () {
   })
 }
 
+jQuery(function(){
+   jQuery(window).scroll(function (){
+       jQuery('.fadein').each(function(){
+           var elemPos = jQuery(this).offset().top;
+           var scroll = jQuery(window).scrollTop();
+           var windowHeight = jQuery(window).height();
+           if (scroll > elemPos - windowHeight + 100){
+               jQuery(this).addClass('scrollin');
+           }
+       });
+   });
+ });
+//clickするとふっとぶ
 $(function(){
   $('a[href^="#"]').click(function(){
     var adjust = -67;
