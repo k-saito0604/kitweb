1d34fda71d700cfd568047d9c07309ab98ac0f40
snemesk <sainemesis.ksk@gmail.com>
2020-03-15 17:07:23 +0900

    contactest

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -4,20 +4,19 @@ const nodemailer = require("nodemailer");
 const createHtmlMail = require("./modules/mail-template"); // this function returns html email code
 
 exports.handler = function(event, context, callback) {
-  //const user = process.env.MAIL_USER;       // some@mail.com
-  //const pass = process.env.MAIL_PASSWORD;   // 42isthecoolestnumber
-  const user = sainemesis.ksk@gmail.com;
-  const pass = Neme1059;
+  const user = process.env.MAIL_USER;       // some@mail.com
+  const pass = process.env.MAIL_PASSWORD;   // 42isthecoolestnumber
+
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: { user, pass }
   });
-  
+
   // Parse data sent in form hook (email, name etc)
   const { data } = JSON.parse(event.body);
-  
+
   // make sure we have data and email
   if (!data || !data.email) {
     return callback(null, {
@@ -25,7 +24,7 @@ exports.handler = function(event, context, callback) {
       body: 'Mailing details not provided'
     })
   }
-  
+
   let mailOptions = {
     from: `"Maciej 🥝 Smoothielicious" <${user}>`,
     to: data.email, // send to email from contact form
