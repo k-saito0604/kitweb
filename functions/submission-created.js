696b8d60981eff988a2edce26c565c1ef38dfbea
snemesk <sainemesis.ksk@gmail.com>
2020-03-16 23:05:45 +0900

    test

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -16,8 +16,8 @@ exports.handler = function(event, context, callback) {
 
   let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {user, pass},
   });
 
