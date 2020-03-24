3f92e6965828d6c638b3952e94cf5cde16650942
snemesk <sainemesis.ksk@gmail.com>
2020-03-24 11:58:09 +0900

    0324

---
+++ functions/submission-createdtest.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+const headers = {
+  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Credentials': true,
+};
+
+exports.handler = function(event, context, callback) {
+  const user = process.env.MAIL_USER;
+  const pass = process.env.MAIL_PASSWORD;
+  const { name, email, message } = JSON.parse(event.body).payload.data
+
+  console.log(user);
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_LOGIN,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN
+    },
+  });
+
+  console.log(event.body);
+
+  let mailOptions = {
+    from: `"" <${user}>`,
+    to: `${email}`,
+    subject: 'フォームを送信いたしました',
+    html: '<p>メッセージを送信しました。</p>',
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: 'Ok',
+      });
+    }
+  });
+};
