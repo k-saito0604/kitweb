6afa4c6375f1aa9b6f5b3b776a99f522f8b1b9c2
snemesk <sainemesis.ksk@gmail.com>
2020-03-25 23:27:29 +0900

    0324

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,31 +1,39 @@
 require('dotenv').config();
 
 const nodemailer = require('nodemailer');
+const OAUTH_PLAYGROUND = ' https://developers.google.com/oauthplayground' ; 
 
-const headers = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
-  'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Credentials': true,
-};
 
 exports.handler = function(event, context, callback) {
-  const user = process.env.MAIL_USER;
-  const pass = process.env.MAIL_PASSWORD;
+  const type = 'OAuth2';
+  const user = process.env.MAIL_LOGIN;
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const refreshToken = process.env.REFRESH_TOKEN;
+  const accessToken = process.env.ACCESS_TOKEN;
   const { name, email, message } = JSON.parse(event.body).payload.data
-
+  
   console.log(user);
 
   let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
-    auth: {user, pass},
+    auth: {
+      type,
+      user,
+      clientId,
+      clientSecret,
+      refreshToken,
+      accessToken,
+      OAUTH_PLAYGROUND
+    },
   });
 
   console.log(event.body);
 
   let mailOptions = {
-    from: `"test" <${user}>`,
+    from: `""<${user}>`,
     to: `${email}`,
     subject: 'フォームを送信いたしました',
     html: '<p>メッセージを送信しました。</p>',
