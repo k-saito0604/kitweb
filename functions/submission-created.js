3d991d56cbe3c4fa689ff3da885c8cee623839db
snemesk <sainemesis.ksk@gmail.com>
2020-03-26 00:19:49 +0900

    0324

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,37 +1,44 @@
 require('dotenv').config();
 
-  const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+
+const headers = {
+  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Credentials': true,
+};
 
 exports.handler = function(event, context, callback) {
+  const user = process.env.MAIL_USER;
+  const pass = process.env.MAIL_PASSWORD;
   const { name, email, message } = JSON.parse(event.body).payload.data
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: process.env.MAIL_LOGIN,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: process.env.ACCESS_TOKEN
-        }
-    });
-    console.log(event.body);
 
-    transporter.sendMail({
-      from: `""<${user}>`,
-      to: `${email}`,
-      subject: 'フォームを送信いたしました',
-      html: '<p>メッセージを送信しました。</p>',
-    }, function(error, info) {
-    	if (error) {
-    		callback(error);
-    	} else {
-    		callback(null, {
-			    statusCode: 200,
-			    body: "Ok"
-	    	});
-    	}
-    });
-}
+  console.log(user);
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {user, pass},
+  });
+
+  console.log(event.body);
+
+  let mailOptions = {
+    from: `"test" <${user}>`,
+    to: `${email}`,
+    subject: 'フォームを送信いたしました',
+    html: '<p>メッセージを送信しました。</p>',
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: 'Ok',
+      });
+    }
+  });
+};
