f53a7cb38c0d3c59dcdaff4811be3ac4436c3a07
snemesk <sainemesis.ksk@gmail.com>
2020-03-25 23:42:58 +0900

    0324

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,50 +1,37 @@
 require('dotenv').config();
 
-const nodemailer = require('nodemailer');
-
+  const nodemailer = require('nodemailer');
+  const { name, email, message } = JSON.parse(event.body).payload.data
 
 exports.handler = function(event, context, callback) {
-  const type = 'OAuth2';
-  const user = process.env.MAIL_LOGIN;
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
-  const refreshToken = process.env.REFRESH_TOKEN;
-  const accessToken = process.env.ACCESS_TOKEN;
-  const { name, email, message } = JSON.parse(event.body).payload.data
-  
-  console.log(user);
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: process.env.MAIL_LOGIN,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: process.env.ACCESS_TOKEN
+        }
+    });
+    console.log(event.body);
 
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      type,
-      user,
-      clientId,
-      clientSecret,
-      refreshToken,
-      accessToken
-    },
-  });
-
-  console.log(event.body);
-
-  let mailOptions = {
-    from: `""<${user}>`,
-    to: `${email}`,
-    subject: 'フォームを送信いたしました',
-    html: '<p>メッセージを送信しました。</p>',
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: 'Ok',
-      });
-    }
-  });
-};
+    transporter.sendMail({
+      from: `""<${user}>`,
+      to: `${email}`,
+      subject: 'フォームを送信いたしました',
+      html: '<p>メッセージを送信しました。</p>',
+    }, function(error, info) {
+    	if (error) {
+    		callback(error);
+    	} else {
+    		callback(null, {
+			    statusCode: 200,
+			    body: "Ok"
+	    	});
+    	}
+    });
+}
